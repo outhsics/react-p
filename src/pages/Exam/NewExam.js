7a22ca730dfb2349f292c91adc339785520ecc72
outhsics <outhsics@gmail.com>
2019-02-22 16:33:13 +0800

    u

--- src/pages/Exam/NewExam.js
+++ src/pages/Exam/NewExam.js
@@ -63,24 +63,7 @@ class NewExam extends PureComponent {
         console.log(file, fileList);
       }
     },
-    defaultFileList: [{
-      uid: '1',
-      name: 'xxx.png',
-      status: 'done',
-      response: 'Server Error 500', // custom error message to show
-      url: 'http://www.baidu.com/xxx.png',
-    }, {
-      uid: '2',
-      name: 'yyy.png',
-      status: 'done',
-      url: 'http://www.baidu.com/yyy.png',
-    }, {
-      uid: '3',
-      name: 'zzz.png',
-      status: 'error',
-      response: 'Server Error 500', // custom error message to show
-      url: 'http://www.baidu.com/zzz.png',
-    }],
+    defaultFileList: [],
   };
 
   constructor(props, context) {
